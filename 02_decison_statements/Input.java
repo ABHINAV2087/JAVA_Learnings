df6e4a011e20c90e77a08e3b5e04cf41c47fa432
ABHINAV2087 <abhinav.tirole2021@vitbhopal.ac.in>
2023-11-30 00:00:04 +0530

    update

--- 02_decison_statements/Input.java
+++ 02_decison_statements/Input.java
@@ -10,19 +10,26 @@ public class Input {
         System.out.println("your age is "+age);
     }
 }
-class Input1 {
+
+// ACCEPTING STRINGS FROM THE USER 
+
+
+ class StringInput {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("what is your age ? ");
-        int age = sc.nextInt();
-        System.out.println("your age is "+age);
+        System.out.println("Enter your Name: ");
+        String name = sc.next();
+        System.out.println("Greetings Of The Day "+name);
     }
-}
-class Input3 {
+}// is method me ye problem aati hai ki jaha bhi input me space aa jata hai waha se ye input nhi leta bas space ke pehle ka hi leta hai  
+
+// to hum dusri method use krenge 
+
+ class StringInput2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("what is your age ? ");
-        int age = sc.nextInt();
-        System.out.println("your age is "+age);
+        System.out.println("Enter your Name: ");
+        String name = sc.nextLine();
+        System.out.println("Greetings Of The Day "+name);
     }
 }
