9025a92515438518d0265b5deb300a69d2346e7b
ABHINAV2087 <abhinav.tirole2021@vitbhopal.ac.in>
2023-12-02 23:11:12 +0530

    update

--- 02_decison_statements/Listprime.java
+++ 02_decison_statements/Listprime.java
@@ -18,3 +18,21 @@ public class Listprime {
         }
     }
 }
+class Listprime2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n ;
+        System.out.print("enter a number : " );
+        n = sc.nextInt();
+        for(int i = 2;i<=n;i++ ){
+            int j;
+            for (j = 2 ;j<=i-1;j++){
+                if(i%j==0)
+                break ;
+            }
+            if(j==i){
+                System.out.println(j);
+            }
+        }
+    }
+}
